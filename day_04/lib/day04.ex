0f2fcf1eb6593712a88d4f44b7ee1f94691d116d
William Fuller <williamfulle@gmail.com>
2024-12-08 12:29:56 +0100

    day4 in progress

--- day_04/lib/day04.ex
+++ day_04/lib/day04.ex
@@ -1,48 +1,70 @@
 defmodule Day04 do
-  def part1 do 
+  def part1 do
     input()
   end
 
   # IO
-  defp input() do 
-    AOC.get_input(4, "session=53616c7465645f5f34803e22ae54be6575342af046e5b0b08fdc88699267a1f0f2700846ff857646bbe27fd78ce33e73cbd840cf32eba4508ea9dc81510eee5a")
+  defp input() do
+    AOC.get_input(
+      4,
+      "session=53616c7465645f5ff44d2dc513e2d20748caf8d1fb506ed7f0f9fa745c45639b445549dba9db95841f5faa7576ed3c84c5bd7c3ca1dcf6b5eacb693b0807012e"
+    )
     |> parse
     |> find(~c"XMAS")
   end
 
-  defp parse(input) do 
-    String.split(input, "\n")
-    |> Enum.with_index
+  defp parse(input) do
+    String.trim(input)
+    |> String.split("\n")
+    |> Enum.with_index()
     |> Enum.flat_map(fn {line, row} ->
       String.to_charlist(line)
-      |> Enum.with_index
+      |> Enum.with_index()
       |> Enum.map(fn {letter, col} ->
-        {{row, col}, letter} 
+        {{row, col}, letter}
       end)
     end)
-    |> Map.new
+    |> Map.new()
   end
 
   # logic
-  defp directions() do 
-    [{-1, -1}, {-1, 0}, {-1, 1},
-    {0, -1}, {0, 1},
-    {1, -1}, {1, 0}, {1, 1}]
+  defp directions() do
+    [{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}]
   end
 
-  defp find(grid, _word) do 
-    Enum.reduce(grid, 0, fn {position, _}, acc -> 
-      case match?(grid, position, ?X) do 
-        true -> acc+1
+  defp find(grid, word) do
+    Enum.reduce(grid, 0, fn {position, _}, acc ->
+      case recursive_find(grid, position, word) do
+        true -> acc + 1
         false -> acc
       end
     end)
   end
 
-  defp match?(grid, position, letter) do 
-     case Map.get(grid, position) do 
-        ^letter -> true
-        _ -> false
-     end
+  defp recursive_find(grid, position, [_ | []]) do
+    case Map.get(grid, position) do
+      nil -> false
+      _ -> true
+    end
+  end
+
+  defp recursive_find(grid, {x, y}, [l | rest]) do
+    case match?(grid, {x, y}, l) do
+      true ->
+        Enum.any?(directions(), fn {x_diff, y_diff} ->
+          position = {x + x_diff, y + y_diff}
+          recursive_find(grid, position, rest)
+        end)
+
+      _ ->
+        false
+    end
+  end
+
+  defp match?(grid, position, char) do
+    case Map.get(grid, position) do
+      ^char -> true
+      _ -> false
+    end
   end
 end
